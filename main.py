1ba028d3aea224035426513490462f1c5451afee
kinga <kingamazur33@gmail.com>
2025-10-04 14:00:16 +0200

    refractor

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import os
+from app import create_app, db
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel_tracker.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
+app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
+        # Import models here to ensure tables are created
+        import models
         db.create_all()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import os
+from app import db
 
-from main import app
-
-db = SQLAlchemy(app)
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     destination = db.Column(db.String(100), nullable=False)
